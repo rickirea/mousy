3b70a2df2cf4243ab3364dc7890e7bbc8b284cb7
Ricardo Rea <rickirea@gmail.com>
2018-06-06 00:43:20 -0500

    Mejora

--- main.js
+++ main.js
@@ -4,15 +4,51 @@ window.onload = function(){
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
 
+  var canvasCuadricula = document.getElementById("canvasCuadricula");
+  var ctxC = canvasCuadricula.getContext("2d");
+
   function Board(){
     this.x = 0;
     this.y = 0;
     this.width = canvas.width;
     this.height = canvas.height;
+    this.img = new Image();
+    this.img.src = "images/pasto1.png ";
+
+    this.img.onload = function(){
+      this.draw();
+    }.bind(this);
 
     this.draw = function(){
-      ctx.fillStyle = 'rgb(61, 60, 60)';
-      ctx.fillRect(this.x, this.y, this.width, this.height);
+      //ctx.fillStyle = 'rgb(61, 60, 60)';
+      //ctx.fillRect(this.x, this.y, this.width, this.height);
+      ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+      //ctx.drawImage(this.img, this.xCloud - this.width, this.y, this.width, this.height);
+    }
+  }
+
+  function Cuadricula(){
+    this.x = 0;
+    this.y = 0;
+    this.sizeWidth = 10;
+    this.sizeHeight = 5;
+    this.width = canvas.width;
+    this.height = canvas.height;
+
+    this.draw = function(){
+      for(let x=0; x <= canvas.width; x += (canvas.width/this.sizeWidth))
+      {
+        ctxC.moveTo(x, 0);
+        ctxC.lineTo(x, canvas.height);
+        ctxC.stroke();
+      }
+
+      for(let y=0; y <= canvas.height; y += (canvas.height/this.sizeHeight))
+      {
+        ctxC.moveTo(0, y);
+        ctxC.lineTo(canvas.width, y);
+        ctxC.stroke();
+      }
     }
   }
 
@@ -31,16 +67,17 @@ window.onload = function(){
   }
 
   function Mousy(){
-    this.x = 1;
-    this.y = 361;
-    this.width = 38;
-    this.height = 38;
+    this.x = 0;
+    this.y = 360;
+    this.width = 80;
+    this.height = 80;
     this.img = new Image();
     this.img.src = "images/mousyUp.png";
     this.isGoingUP = true;
     this.isGoingDown = false;
     this.isGoingLeft = false;
     this.isGoingRight = false;
+    this.speed = 1;
     
     //this.sound = new Audio();
     //this.sound.src = "http://soundfxcenter.com/video-games/super-mario-bros/8d82b5_Super_Mario_Bros_Jump_Super_Sound_Effect.mp3";
@@ -55,88 +92,205 @@ window.onload = function(){
 
       if(this.y <= 0)
       {
-        this.dontGoUp(0,0);
+        this.dontGoUp(0);
         //gameOver();
       }
       if(this.y >= canvas.height - this.height)
       {
-        this.dontGoDown(canvas.width, canvas.height);
+        this.dontGoDown(canvas.height);
       }
 
       if(this.x <= 0)
       {
-        this.dontGoLeft(0,0);
+        this.dontGoLeft(0);
         //gameOver();
       }
       if(this.x >= canvas.width - this.width)
       {
-        this.dontGoRight(canvas.width , canvas.height);
+        this.dontGoRight(canvas.width);
       }
     }
 
-    this.dontGoUp = function(pipeX, pipeY){
-      this.y = pipeY + 1;
+    this.dontGoUp = function(pipeY){
+      this.y = pipeY;
     }
 
-    this.dontGoDown = function(pipeX, pipeY){
-      this.y = pipeY - this.height -1;
+    this.dontGoDown = function(pipeY){
+      this.y = pipeY - this.height;
     }
 
-    this.dontGoLeft = function(pipeX, pipeY){
-      this.x = pipeX + 1;
+    this.dontGoLeft = function(pipeX){
+      this.x = pipeX;
     }
 
-    this.dontGoRight = function(pipeX, pipeY){
-      this.x = pipeX - this.width - 1;
+    this.dontGoRight = function(pipeX){
+      this.x = pipeX - this.width;
     }
 
     this.moveUp = function(){
-      this.img.src = "images/mousyUp.png";
-      this.y -= 1;
-      
-      this.isGoingUP = true;
-      this.isGoingDown = false;
-      this.isGoingLeft = false;
-      this.isGoingRight = false;
-
+      if(!this.isGoingUP)
+      {
+      //   this.y -= this.speed;
+      // }
+      // else
+      // {
+        this.img.src = "images/mousyUp.png";
+        this.isGoingUP = true;
+        this.isGoingDown = false;
+        this.isGoingLeft = false;
+        this.isGoingRight = false;
+      }
       //this.sound.pause();
       //this.sound.play();
     }
 
     this.moveDown = function(){
-      this.img.src = "images/mousyDown.png";
-      this.y += 1;
-
-      this.isGoingUP = false;
-      this.isGoingDown = true;
-      this.isGoingLeft = false;
-      this.isGoingRight = false;
+      if(!this.isGoingDown)
+      {
+      //   this.y += this.speed;
+      // }
+      // else
+      // {
+        this.img.src = "images/mousyDown.png";
+        this.isGoingUP = false;
+        this.isGoingDown = true;
+        this.isGoingLeft = false;
+        this.isGoingRight = false;
+      }
       //this.sound.pause();
       //this.sound.play();
     }
 
     this.moveLeft = function(){
-      this.img.src = "images/mousyLeft.png";
-      this.x -= 1;
-
-      this.isGoingUP = false;
-      this.isGoingDown = false;
-      this.isGoingLeft = true;
-      this.isGoingRight = false;
+      if(!this.isGoingLeft)
+      {
+      //   this.x -= this.speed;
+      // }
+      // else
+      // {
+        this.img.src = "images/mousyLeft.png";
+        this.isGoingUP = false;
+        this.isGoingDown = false;
+        this.isGoingLeft = true;
+        this.isGoingRight = false;
+      }
       //this.sound.pause();
       //this.sound.play();
     }
 
     this.moveRight = function(){
-      this.img.src = "images/mousyRight.png";
-      this.x += 1;
-
-      this.isGoingUP = false;
-      this.isGoingDown = false;
-      this.isGoingLeft = false;
-      this.isGoingRight = true;
+      if (!this.isGoingRight)
+      {
+      //   this.x += this.speed;
+      // }
+      // else
+      // {
+        this.img.src = "images/mousyRight.png";
+        this.isGoingUP = false;
+        this.isGoingDown = false;
+        this.isGoingLeft = false;
+        this.isGoingRight = true;       
+      }
       //this.sound.pause();
       //this.sound.play();
+    }
+
+    this.turnLeft = function(){
+      if(this.isGoingUP)
+      {
+        this.img.src = "images/mousyLeft.png";
+        this.isGoingUP = false;
+        this.isGoingDown = false;
+        this.isGoingLeft = true;
+        this.isGoingRight = false; 
+        return;
+      }
+      if(this.isGoingDown)
+      {
+        this.img.src = "images/mousyRight.png";
+        this.isGoingUP = false;
+        this.isGoingDown = false;
+        this.isGoingLeft = false;
+        this.isGoingRight = true; 
+        return;
+      }
+      if(this.isGoingLeft)
+      {
+        this.img.src = "images/mousyDown.png";
+        this.isGoingUP = false;
+        this.isGoingDown = true;
+        this.isGoingLeft = false;
+        this.isGoingRight = false; 
+        return;
+      }
+      if (this.isGoingRight)
+      {
+        this.img.src = "images/mousyUp.png";
+        this.isGoingUP = true;
+        this.isGoingDown = false;
+        this.isGoingLeft = false;
+        this.isGoingRight = false; 
+        return;
+      } 
+    }
+
+    this.turnRight = function(){
+      if(this.isGoingUP)
+      {
+        this.img.src = "images/mousyRight.png";
+        this.isGoingUP = false;
+        this.isGoingDown = false;
+        this.isGoingLeft = false;
+        this.isGoingRight = true; 
+        return;
+      }
+      if(this.isGoingDown)
+      {
+        this.img.src = "images/mousyLeft.png";
+        this.isGoingUP = false;
+        this.isGoingDown = false;
+        this.isGoingLeft = true;
+        this.isGoingRight = false; 
+        return;
+      }
+      if(this.isGoingLeft)
+      {
+        this.img.src = "images/mousyUp.png";
+        this.isGoingUP = true;
+        this.isGoingDown = false;
+        this.isGoingLeft = false;
+        this.isGoingRight = false; 
+        return;
+      }
+      if (this.isGoingRight)
+      {
+        this.img.src = "images/mousyDown.png";
+        this.isGoingUP = false;
+        this.isGoingDown = true;
+        this.isGoingLeft = false;
+        this.isGoingRight = false; 
+        return;
+      }
+    }
+
+    this.moveFordward = function()
+    {
+      if(this.isGoingUP)
+      {
+        this.y -= this.speed;
+      }
+      if(this.isGoingDown)
+      {
+        this.y += this.speed;
+      }
+      if(this.isGoingLeft)
+      {
+        this.x -= this.speed;
+      }
+      if (this.isGoingRight)
+      {
+        this.x += this.speed;
+      } 
     }
 
     this.isTouching = function(pipe){
@@ -200,10 +354,10 @@ window.onload = function(){
   }
 
   function Queso(){
-    this.x = 361;
-    this.y = 1;
-    this.width = 38;
-    this.height = 38;
+    this.x = 720;
+    this.y = 0;
+    this.width = 80;
+    this.height = 80;
     this.img = new Image();
     this.img.src = "images/queso.png";
     
@@ -238,32 +392,279 @@ window.onload = function(){
     // }
   }
 
+  function Trap(){
+    this.x = 240;
+    this.y = 160;
+    this.width = 80;
+    this.height = 80;
+    this.img = new Image();
+    this.img.src = "images/mousetrap2.png";
+    
+    //this.sound = new Audio();
+    //this.sound.src = "http://soundfxcenter.com/video-games/super-mario-bros/8d82b5_Super_Mario_Bros_Jump_Super_Sound_Effect.mp3";
+
+    this.img.onload = function(){
+      this.draw();
+    }.bind(this);
+
+    this.draw = function(){
+      //this.y += 2;
+      ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+
+      // if(this.y < 0 || this.y > canvas.height - this.height)
+      // {
+      //   gameOver();
+      // }
+    }
+
+    this.move = function(){
+      this.y -= 50;
+      //this.sound.pause();
+      //this.sound.play();
+    }.bind(this);
+
+    // this.isTouching = function(pipe){
+    //   return (this.x < pipe.x + pipe.width) 
+    //       && (this.x + this.width > pipe.x)
+    //       && (this.y < pipe.y + pipe.height)
+    //       && (this.y + this.height > pipe.y);
+    // }
+  }
+
+  function Snake(){
+    this.x = 480;
+    this.y = 240;
+    this.width = 80;
+    this.height = 80;
+    this.img = new Image();
+    this.img.src = "images/snake.png";
+    
+    //this.sound = new Audio();
+    //this.sound.src = "http://soundfxcenter.com/video-games/super-mario-bros/8d82b5_Super_Mario_Bros_Jump_Super_Sound_Effect.mp3";
+
+    this.img.onload = function(){
+      this.draw();
+    }.bind(this);
+
+    this.draw = function(){
+      //this.y += 2;
+      ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+
+      // if(this.y < 0 || this.y > canvas.height - this.height)
+      // {
+      //   gameOver();
+      // }
+    }
+
+    this.move = function(){
+      this.y -= 50;
+      //this.sound.pause();
+      //this.sound.play();
+    }.bind(this);
+
+    // this.isTouching = function(pipe){
+    //   return (this.x < pipe.x + pipe.width) 
+    //       && (this.x + this.width > pipe.x)
+    //       && (this.y < pipe.y + pipe.height)
+    //       && (this.y + this.height > pipe.y);
+    // }
+  }
+
+  function Texture(x,y){
+    this.x = x;
+    this.y = y;
+    this.width = 80;
+    this.height = 80;
+    this.img = new Image();
+    this.img.src = "images/stone7.jpg";
+    
+    //this.sound = new Audio();
+    //this.sound.src = "http://soundfxcenter.com/video-games/super-mario-bros/8d82b5_Super_Mario_Bros_Jump_Super_Sound_Effect.mp3";
+
+    this.img.onload = function(){
+      this.draw();
+    }.bind(this);
+
+    this.draw = function(){
+      //this.y += 2;
+      ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+
+      // if(this.y < 0 || this.y > canvas.height - this.height)
+      // {
+      //   gameOver();
+      // }
+    }
+
+    this.move = function(){
+      this.y -= 50;
+      //this.sound.pause();
+      //this.sound.play();
+    }.bind(this);
+
+    // this.isTouching = function(pipe){
+    //   return (this.x < pipe.x + pipe.width) 
+    //       && (this.x + this.width > pipe.x)
+    //       && (this.y < pipe.y + pipe.height)
+    //       && (this.y + this.height > pipe.y);
+    // }
+  }
+
+  function Bridge(x,y){
+    this.x = x;
+    this.y = y;
+    this.width = 80;
+    this.height = 80;
+    this.img = new Image();
+    this.img.src = "images/wood.png";
+    
+    //this.sound = new Audio();
+    //this.sound.src = "http://soundfxcenter.com/video-games/super-mario-bros/8d82b5_Super_Mario_Bros_Jump_Super_Sound_Effect.mp3";
+
+    this.img.onload = function(){
+      this.draw();
+    }.bind(this);
+
+    this.draw = function(){
+      //this.y += 2;
+      ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+
+      // if(this.y < 0 || this.y > canvas.height - this.height)
+      // {
+      //   gameOver();
+      // }
+    }
+
+    this.move = function(){
+      this.y -= 50;
+      //this.sound.pause();
+      //this.sound.play();
+    }.bind(this);
+
+    // this.isTouching = function(pipe){
+    //   return (this.x < pipe.x + pipe.width) 
+    //       && (this.x + this.width > pipe.x)
+    //       && (this.y < pipe.y + pipe.height)
+    //       && (this.y + this.height > pipe.y);
+    // }
+  }
+
+  function BrokeStone(x,y){
+    this.x = x;
+    this.y = y;
+    this.width = 80;
+    this.height = 80;
+    this.img = new Image();
+    this.img.src = "images/brokeStone.png";
+    
+    //this.sound = new Audio();
+    //this.sound.src = "http://soundfxcenter.com/video-games/super-mario-bros/8d82b5_Super_Mario_Bros_Jump_Super_Sound_Effect.mp3";
+
+    this.img.onload = function(){
+      this.draw();
+    }.bind(this);
+
+    this.draw = function(){
+      //this.y += 2;
+      ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+
+      // if(this.y < 0 || this.y > canvas.height - this.height)
+      // {
+      //   gameOver();
+      // }
+    }
+
+    this.move = function(){
+      this.y -= 50;
+      //this.sound.pause();
+      //this.sound.play();
+    }.bind(this);
+
+    // this.isTouching = function(pipe){
+    //   return (this.x < pipe.x + pipe.width) 
+    //       && (this.x + this.width > pipe.x)
+    //       && (this.y < pipe.y + pipe.height)
+    //       && (this.y + this.height > pipe.y);
+    // }
+  }
+
+  function Tree(x,y){
+    this.x = x;
+    this.y = y;
+    this.width = 80;
+    this.height = 80;
+    this.img = new Image();
+    this.img.src = "images/tree.png";
+    
+    //this.sound = new Audio();
+    //this.sound.src = "http://soundfxcenter.com/video-games/super-mario-bros/8d82b5_Super_Mario_Bros_Jump_Super_Sound_Effect.mp3";
+
+    this.img.onload = function(){
+      this.draw();
+    }.bind(this);
+
+    this.draw = function(){
+      //this.y += 2;
+      ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+
+      // if(this.y < 0 || this.y > canvas.height - this.height)
+      // {
+      //   gameOver();
+      // }
+    }
+
+    this.move = function(){
+      this.y -= 50;
+      //this.sound.pause();
+      //this.sound.play();
+    }.bind(this);
+
+    // this.isTouching = function(pipe){
+    //   return (this.x < pipe.x + pipe.width) 
+    //       && (this.x + this.width > pipe.x)
+    //       && (this.y < pipe.y + pipe.height)
+    //       && (this.y + this.height > pipe.y);
+    // }
+  }
+
   //declaraciones
   var board = new Board();
   var mousy = new Mousy();
   var queso = new Queso();
+  var mouseTrap = new Trap();
+  var snake = new Snake();
+  var cuadricula = new  Cuadricula();
+  var bridge = new Bridge(160,80);
+  var bridge2 = new Bridge(640,160);
+  var brokeStone = new BrokeStone(160,80);
+  var tree = new Tree(320,80);
   var intervalo;
   var frames = 0;
   var pipes = [];
+  var textures = [];
+  var stepCount =80;
 
   //listeners
   //listener
   addEventListener('keydown', function(e){
     if(e.keyCode === 37)
     {
-      mousy.moveLeft();
+      mousy.turnLeft();
+      //stepCount=0;
     }
     if(e.keyCode === 38)
     {
-      mousy.moveUp();
+      //mousy.moveUp();
+      stepCount=0;
     }
     if(e.keyCode === 39)
     {
-      mousy.moveRight();
+      mousy.turnRight();
+      //stepCount=0;
     }
     if(e.keyCode === 40)
     {
-      mousy.moveDown();
+      //mousy.moveDown();
+      //stepCount=0;
     }
   });
 
@@ -280,7 +681,7 @@ window.onload = function(){
 
   function stop(){
     clearInterval(intervalo);
-    intervalo = 0;
+    intervalo = 1;
     //board.sound.pause();
     //board.soundGameOver.play();
   }
@@ -329,6 +730,35 @@ window.onload = function(){
         //gameOver();
       }
     });
+  }
+
+  function generateTextures(){
+    textures.push(new Texture(0,320));
+    textures.push(new Texture(0,240));
+    textures.push(new Texture(0,160));
+    textures.push(new Texture(80,160));
+    textures.push(new Texture(80,80));
+    //textures.push(new Texture(160,80));
+    textures.push(new Texture(240,80));
+    textures.push(new Texture(240,0));
+    textures.push(new Texture(240,160));
+    textures.push(new Texture(240,240));
+    textures.push(new Texture(320,240));
+    textures.push(new Texture(320,0));
+    textures.push(new Texture(400,0));
+    textures.push(new Texture(320,320));
+    textures.push(new Texture(400,320));
+    textures.push(new Texture(400,80));
+    textures.push(new Texture(480,80));
+    textures.push(new Texture(480,320));
+    textures.push(new Texture(480,240));
+    textures.push(new Texture(560,240));   
+    textures.push(new Texture(560,80));
+    textures.push(new Texture(560,160)); 
+    //textures.push(new Texture(640,160));
+    textures.push(new Texture(720,160));  
+    textures.push(new Texture(720,80));
+    textures.push(new Texture(720,0));  
   }
 
   function generatePipes(){
@@ -395,22 +825,59 @@ window.onload = function(){
     });
   }
 
+  function drawTextures(){
+    //ctx.strokeStyle = "white";
+    textures.forEach(function(texture){
+      texture.draw();
+    });
+  }
+
+
+  function checkForWalk(){
+    // if(!(frames%10 === 0))
+    // {
+    //   return;
+    // }
+
+    if(stepCount < 80)
+    {
+      mousy.moveFordward();
+      stepCount += 1;
+    }
+  }
+
   function update(){
     frames++;
     console.log("frame: " + frames);
-    ctx.clearRect(0,0,canvas.width, canvas.height);
+    //ctx.clearRect(0,0,canvas.width, canvas.height);
 
     board.draw();
-    drawPipes();
-    mousy.draw();
-    console.log(mousy.x+","+mousy.y);
-    queso.draw();
+    drawTextures();
+    //brokeStone.draw();
+    bridge.draw();
+    bridge2.draw();
+    tree.draw();
 
-    checkColition();
+    ///cuadricula.draw();
+    //drawPipes();
+    mousy.draw();
+    
+    console.log(mousy.x+","+mousy.y);
+    
+    queso.draw();
+    mouseTrap.draw();
+    snake.draw();
+
+    //checkColition();
+
+    checkForWalk();
   }
 
   function startGame(){
-    generatePipes();
+    //generatePipes();
+    //drawPipes();
+    generateTextures();
+    cuadricula.draw();
 
     if(intervalo > 0)
     {
